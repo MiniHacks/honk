e35cba2c1f0fee04bc3cba8e2b5c1904e873de4a
Samyok Nepal <samyok@fiveable.me>
2022-02-20 00:23:00 -0600

    add crx

--- crx/after.js
+++ crx/after.js
@@ -1,4 +1,16 @@
 // this code will be executed after page load
 (function() {
   console.log('after.js executed');
+  console.log(getText())
+  setInterval(() => {
+    console.log(getText())
+  }, 30 * 1000)
 })();
+
+function getText() {
+  return {
+    title: document.querySelector("title")?.innerText,
+    header: document.querySelector("h1")?.innerText,
+    content: document.querySelector("body")?.innerText
+  }
+}
