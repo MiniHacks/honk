2303ec8da68fecd894a32e2a6f10d642728e20d3
Samyok Nepal <samyok@fiveable.me>
2022-02-19 22:30:39 -0600

    push firebase

--- nextjs/pages/api/hello.js
+++ nextjs/pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import admin from '../firebase/nodeApp'
 
 export default function hello(req, res) {
+    // https://github.com/vercel/next.js/blob/canary/examples/with-firebase/fetchData/getProfileData.js
+    const db = admin.firestore()
+
     res.status(200).json({ name: "John Doe" });
 }
