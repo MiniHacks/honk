65913967ff8ef1ee8f818e9d3b43601906d1fb6f
iCalculated <fufa0001@gmail.com>
2022-02-19 21:33:44 -0600

    demo routes

--- python/python_backend/app.py
+++ python/python_backend/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from random import choice
+from random import choice, random
 from typing import List
 
 app = FastAPI()
@@ -8,11 +8,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/api/topics")
-def topics(el):
-    title = el.title
-    header = el.header
-    content = el.content
+@app.get("/topics")
+def topics(el: List[str]):
+    title, header, content = el
     return list(set([choice(title), choice(header), choice(content), choice(content)]))
 
-
+@app.get("/distracted")
+def distracted(topics: List[str]):
+    return random() > 0.3
