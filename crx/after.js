0b25e6595d77e49f3da66041f3a8a78925c24415
Samyok Nepal <samyok@fiveable.me>
2022-02-20 07:43:42 -0600

    add vrything

--- crx/after.js
+++ crx/after.js
@@ -8,9 +8,22 @@
 })();
 
 function getText() {
-  return {
+  if (location.href.includes("google.com")) return;
+  const data = {
     title: document.querySelector("title")?.innerText,
     header: document.querySelector("h1")?.innerText,
     content: document.querySelector("body")?.innerText
-  }
+  };
+  const formData = new FormData();
+  formData.append("title", document.querySelector("title")?.innerText);
+  formData.append("header", document.querySelector("h1")?.innerText);
+  formData.append("content", document.querySelector("body")?.innerText);
+  fetch("http://localhost:5001/api/isdistracted", {
+    // header: {"Content-Type": "application/json"},
+    method: "POST",
+    body: formData
+   }).then(r => r.json()).then(r => {
+     console.log(r);
+  })
+  return;
 }
--- nextjs/pages/api/isdistracted.js
+++ nextjs/pages/api/isdistracted.js
@@ -6,7 +6,7 @@ async function getVectors(user) {
     let arr = []
     let time = new Date()
     col.docs.forEach( doc => {
-        
+
         // 5 minutes in miliseconds.
         if(time - doc.data()['timestamp'] > 1000 * 60 * 5) {
             // doc.ref.delete()
@@ -48,29 +48,28 @@ async function addTopics(user, topics) {
 
 export default async function isdistracted(req, res) {
     let user = "c5KKbUvTRO77uXpdEA5Q" // req.body['user']
-    let ele = {topic: "hi", header: "bye", content: "mary"} // req.body['element']
-
-    let previous = await getPreviousVector(user)
+    console.log(req.body);
+    let ele = JSON.parse(req.body).data
+    let previous = JSON.stringify(await getPreviousVector(user))
 
     console.log("Request Contents:")
-    console.log("Elements: " + ele)
-    console.log("Previous: " + previous)
-    // let response = await fetch("http://localhost/python/embeddings", {headers: {"Content-Type":"application/json"}, method: "POST", body: JSON.stringify({
-    //     element: ele,
-    //     previous: previous
-    // })});
+    // console.log("Elements: ", ele);
+    // console.log("Previous: ",  previous)
+    let response = await fetch("http://localhost:5001/python/embeddings", {headers: {"Content-Type":"application/json"}, method: "POST", body: JSON.stringify({
+        element: ele,
+        previous: previous
+    })});
 
-    // let data = response.json();
-    let newVec = [0.2, 0.4, 0.01, 0.32222] // data['vector']
-    let distracted = true // data['distracted']
-    let topics = ['geese boys', 'yotsuba'] // data['keywords']
+    let data = await response.json();
+    let newVec = data['embed_string']
+    let distracted = data['focused']
+    let topics = data['keywords']
 
-    addTopics(user, topics)
-    addVector(user, newVec)
-    let data = { distracted: distracted }
+    await addTopics(user, topics);
+    await addVector(user, newVec);
     if (distracted) {
-        data['distracted_by'] = topics[0] 
+        data['distracted_by'] = topics[0]
     }
-
-    res.status(200).json(data);
+    console.log(Object.keys(data));
+    res.status(200).json(Object.keys(data));
 }
