e00f8eacf62137ae405a000adb4896e88b6d9da1
iCalculated <fufa0001@gmail.com>
2022-02-19 22:07:25 -0600

    yake for keyword extraction

--- python/python_backend/app.py
+++ python/python_backend/app.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from random import choice, random
 from typing import List
-import yake
+from yake import KeywordExtractor
 
 app = FastAPI()
+
+yake_params = {
+    "lan": "en",
+    "dedupLim": 0.9, # try 0.1 to distil sets?
+    "top": 10
+}
+phrase_extractor = KeywordExtractor(max_ngram_size=3, **yake_params)
+word_extractor = KeywordExtractor(max_ngram_size=1, **yake_params)
 
 @app.get("/")
 async def root():
@@ -11,7 +19,19 @@ async def root():
 
 @app.get("/topics")
 def topics(title: str, header: str, content: str):
-    return list(set([choice(title), choice(header), choice(content), choice(content)]))
+    full_text = title + header + content
+    yake_phrases = phrase_extractor.extract_keywords(full_text)
+    yake_words = word_extractor.extract_keywords(full_text)
+
+    print(f"{yake_phrases=}")
+    print(f"{yake_words=}")
+
+    yake_phrases = list(map(lambda x: x[1], filter(lambda x: x[0] < 0.1, phrase_extractor.extract_keywords(full_text))))
+    yake_words = list(map(lambda x: x[1], filter(lambda x: x[0] < 0.1, word_extractor.extract_keywords(full_text))))
+    print(f"{yake_phrases=}")
+    print(f"{yake_words=}")
+
+    return list(set(yake_phrases + yake_words))
 
 @app.get("/distracted")
 def distracted(topics: List[str]):
