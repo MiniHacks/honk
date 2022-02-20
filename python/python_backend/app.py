5b497ef307be09d5b446917c504930d608150273
iCalculated <fufa0001@gmail.com>
2022-02-19 22:15:54 -0600

    too much time on formatting

--- python/python_backend/app.py
+++ python/python_backend/app.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from random import choice, random
 from typing import List
+
 from yake import KeywordExtractor
-
-app = FastAPI()
-
 yake_params = {
     "lan": "en",
     "dedupLim": 0.9, # try 0.1 to distil sets?
     "top": 10
 }
+
+import spacy
+nlp = spacy.load("en_core_sci_lg")
+
+app = FastAPI()
 phrase_extractor = KeywordExtractor(max_ngram_size=3, **yake_params)
 word_extractor = KeywordExtractor(max_ngram_size=1, **yake_params)
 
@@ -18,20 +21,28 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/topics")
-def topics(title: str, header: str, content: str):
+def topics(title: str, content: str, header: str = ""):
     full_text = title + header + content
+    # ==== YAKE! ====
+    print("==== YAKE! ====")
     yake_phrases = phrase_extractor.extract_keywords(full_text)
     yake_words = word_extractor.extract_keywords(full_text)
-
     print(f"{yake_phrases=}")
     print(f"{yake_words=}")
 
+    # ==== SpaCy ====
+    print("==== SpaCy =====")
+    doc = nlp(full_text)
+    ents = doc.ents
+    print(f"{ents=}")
+
+    # ==== Processing ====
     yake_phrases = list(map(lambda x: x[1], filter(lambda x: x[0] < 0.1, phrase_extractor.extract_keywords(full_text))))
     yake_words = list(map(lambda x: x[1], filter(lambda x: x[0] < 0.1, word_extractor.extract_keywords(full_text))))
-    print(f"{yake_phrases=}")
-    print(f"{yake_words=}")
+    print(f"processed {yake_phrases=}")
+    print(f"processed {yake_words=}")
 
-    return list(set(yake_phrases + yake_words))
+    return list(set(yake_phrases + yake_words + ents))
 
 @app.get("/distracted")
 def distracted(topics: List[List[str]]):
