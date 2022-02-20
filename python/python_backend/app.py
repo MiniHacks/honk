597fa14ac8350d9c6b4500105e14511e0cc44025
iCalculated <fufa0001@gmail.com>
2022-02-19 21:28:59 -0600

    topics test route

--- python/python_backend/app.py
+++ python/python_backend/app.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from random import choice
+from typing import List
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/api/topics")
+def topics(el):
+    title = el.title
+    header = el.header
+    content = el.content
+    return list(set([choice(title), choice(header), choice(content), choice(content)]))
+
+
